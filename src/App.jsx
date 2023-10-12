b99831ec2d34da680a24c6f02fe4bd4244ab0586
Rahul4dev <rahulsinghsignin@gmail.com>
2023-10-12 18:15:22 +0530

    Header section and First Run Dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { About, Home, Profile, SignIn, SignOut } from './pages';
+import { Header } from './components';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
