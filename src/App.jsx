1cb0c2c1d6a7557c783a129a84a211478f663ee9
Rahul4dev <rahulsinghsignin@gmail.com>
2023-10-12 17:51:02 +0530

    Create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About, Home, Profile, SignIn, SignOut } from './pages';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import About from './About';
+import Home from './Home';
+import Profile from './Profile';
+import SignIn from './SignIn';
+import SignOut from './SignOut';
+
+export { Home, Profile, SignIn, SignOut, About };
