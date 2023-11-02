866e17977d8822fcbe553b6e7c1bfe54c573d604
Rahul4dev <rahulsinghsignin@gmail.com>
2023-11-02 06:49:30 +0530

    Create search API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   getListing,
+  getAllListings,
   createListing,
   deleteListing,
   updateListing,
@@ -10,6 +11,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/get/:id', getListing);
+router.get('/get', getAllListings);
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
