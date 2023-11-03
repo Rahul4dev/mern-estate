fa4fd16a1fefd56c5c0c393cbc71da1dd060036d
Rahul4dev <rahulsinghsignin@gmail.com>
2023-11-03 22:59:02 +0530

    add pre-installation settings

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 
+import path from 'path';
+
 dotenv.config();
 
 mongoose
@@ -18,6 +20,7 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -31,6 +34,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist/')));
+
+app.get('*', (req, res) => {
+  res.sendfile(path.join(__dirname, '/client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
